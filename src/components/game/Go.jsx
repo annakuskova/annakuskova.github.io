a53e3c788adb52e412ec866ddfdab221124be1a5
annakuskova <kuskovaau@rambler.ru>
2022-04-18 16:14:13 +0300

    btn back in Game.jsx

--- src/components/game/Go.jsx
+++ src/components/game/Go.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import arrow from "../../Arrow.png";
 
 const Go = () => {
   return (
     <div className="main">
       <div className="nav_btn_back">
-          <a href="/game">back</a>
-        </div>
+        <a href="/game">
+          <img src={arrow} alt="arrow"></img>
+        </a>
+      </div>
       <div className="game">
         <div className="time">5:00</div>
         <div className="camera">camera</div>
-        
+
         <div className="score">5/10</div>
       </div>
     </div>
