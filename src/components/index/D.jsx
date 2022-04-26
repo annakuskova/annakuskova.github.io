e7771959cb14b785b5d51410b352b9a059c2e378
annakuskova <kuskovaau@rambler.ru>
2022-04-26 11:53:40 +0300

    description and photo A - N

--- src/components/index/D.jsx
+++ src/components/index/D.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Content.css";
+import ImgD from "../../images/D.JPG";
+
 
 const D = () => {
-    return <div className="description">
-        it's B!
+    return <div className="content">
+    <div className="sign">
+      <img src={ImgD} alt="A"></img>
     </div>
+    <div className="description">the index finger is straightened,
+     the rest are pressed against the palm.</div>
+  </div>;
 }
 
 export default D;
